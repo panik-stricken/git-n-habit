5a0820e4f1234e271e1f530112fbc84d4feebd19
M.Sterling <giazi.llc@gmail.com>
2021-01-05 08:54:00 -0600

    Nothin

--- studentRecords/srApp.js
+++ studentRecords/srApp.js
@@ -1,13 +1,38 @@
 
 const navigation = {
-    Students: {
-        type: 'Top'
-    },
-    Courses: {
-        type: 'Top'
-    },
     Student: {
-        type: 'Side'
+        type: 'Top',
+        settings: [
+            "Jayden",
+            "Tat'yana",
+            "Domenick",
+            "G",
+            "David",
+            "Guest"
+        ]
+    },
+    Class: {
+        type: 'Top',
+        settings: [
+            "English",
+            "Math",
+            "F. Lang.",
+            "Music",
+            "Ind. Living"
+        ]
+    },
+    Assignment: {
+        type: 'Top',
+        settings: [
+            "New",
+            "Grade"
+        ]
+    },
+    Progress: {
+        type: 'Top',
+        settings: [
+            
+        ]
     }
 }
 
@@ -17,11 +42,19 @@ new Vue({
     el: "#display",
     data: {
         message: "Connect",
-        navTabs: navigation
+        navTabs: navigation,
+        currentTab: null
     },
     computed: {
         primary(){
             return Object.keys(this.navTabs).filter(e=>this.navTabs[e].type === "Top")
+        },
+        panelSettings(){
+            if(this.currentTab !== null){
+                return this.navTabs[this.currentTab].settings;
+            } else {
+                return []
+            }
         }
     }
 
